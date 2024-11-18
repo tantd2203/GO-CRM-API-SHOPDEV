832147f1f2715b03eeb303f84a2f62d8e4e547cf
tan2203 <tranduytan.220300@gmail.com>
2024-11-18 21:32:13 +0700

    handlder connection mysql

--- global/global.go
+++ global/global.go
@@ -3,14 +3,11 @@ package global
 import (
 	"GO-CRM-API-SHOPDEV/pkg/logger"
 	"GO-CRM-API-SHOPDEV/pkg/settings"
+	"gorm.io/gorm"
 )
 
 var (
 	Config settings.Config
 	Logger *logger.LoggerZap
+	Mdb    *gorm.DB
 )
-
-//*
-//
-//
-//*/
--- internal/initalize/mysql.go
+++ internal/initalize/mysql.go
@@ -1,5 +1,62 @@
 package initalize
 
-func InitMysql() {
+import (
+	"GO-CRM-API-SHOPDEV/global"
+	"GO-CRM-API-SHOPDEV/po"
+	"fmt"
+	"gorm.io/driver/mysql"
+	"time"
 
+	"go.uber.org/zap"
+	"gorm.io/gorm"
+)
+
+func checkErrorPanic(err error, errString string) {
+	if err != nil {
+		global.Logger.Error(errString, zap.Error(err))
+		panic(err)
+	}
+}
+func InitMysql() {
+	m := global.Config.Mysql
+
+	fmt.Printf("Username: %s, Password: %s, Host: %s, Port: %d, Dbname: %s\n",
+		m.Username, m.Password, m.Host, m.Port, m.Dbname)
+
+	dsn := "%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local"
+
+	var s = fmt.Sprintf(dsn, m.Username, m.Password, m.Host, m.Port, m.Dbname)
+	fmt.Println("Connection String:", s)
+
+	db, err := gorm.Open(mysql.Open(s), &gorm.Config{
+		SkipDefaultTransaction: false,
+	})
+	checkErrorPanic(err, "InitMysql initialization error")
+	global.Logger.Info("InitMysql initialization success")
+	global.Mdb = db
+
+	SetPool()
+	migrateTables()
+}
+func SetPool() {
+	sqlDb, err := global.Mdb.DB()
+	m := global.Config.Mysql
+	if err != nil {
+		fmt.Printf("mysql error %s:", err)
+	}
+	// set config ~~ Pool
+	sqlDb.SetConnMaxLifetime(time.Duration(m.MaxIdleConns))
+	sqlDb.SetMaxOpenConns(m.MaxOpenConns)
+	sqlDb.SetConnMaxLifetime(time.Duration(m.ConnMaxLifetime))
+}
+
+// gen table database
+func migrateTables() {
+	err := global.Mdb.AutoMigrate(
+		&po.User{},
+		&po.Role{},
+	)
+	if err != nil {
+		fmt.Println("Migrating tables error")
+	}
 }
